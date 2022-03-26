eda3a3ad13b4c980725a1d41061269fcdcec58da
chen-coding <1290389765@qq.com>
2022-03-26 09:57:34 +0800

    Site updated: 2022-03-26 09:57:34

--- dist/music.js
+++ dist/music.js
@@ -2,14 +2,12 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
-    lrcType: 3,
     audio: [
       {
         name: "Miracles",
         artist: 'Coldplay',
         url: 'http://music.163.com/song/media/outer/url?id=29802587.mp3',
         cover: 'http://p3.music.126.net/JpsUkt18Cc4JFWDZCEaAQw==/6672936070254338.jpg?param=300x300',
-        lrc: 'Miracles-Coldplay.lrc'
       },
 	  {
 		name: 'imagine',
