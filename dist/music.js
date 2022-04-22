05212f37bf6ef34fa194fd09af93204d8889a999
chen-coding <1290389765@qq.com>
2022-04-22 18:06:43 +0800

    Site updated: 2022-04-22 18:06:42

--- dist/music.js
+++ dist/music.js
@@ -2,18 +2,14 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
+    lrcType: 3,
     audio: [
       {
-        name: "Miracles",
-        artist: 'Coldplay',
-        url: 'http://music.163.com/song/media/outer/url?id=29802587.mp3',
-        cover: 'http://p3.music.126.net/JpsUkt18Cc4JFWDZCEaAQw==/6672936070254338.jpg?param=300x300',
-      },
-	  {
-		name: 'imagine',
-		artist: 'John Lennon',
-		url: 'http://music.163.com/song/media/outer/url?id=1476431.mp3',
-		cover: 'http://p3.music.126.net/bNfLt8ezFqjYySr0erO73Q==/799344953443960.jpg?param=300x300',
-	  }
+        name: "再唱一次这首歌好吗？",
+        artist: '鹿先森乐队',
+        url: 'http://music.163.com/song/media/outer/url?id=1474654265.mp3',
+        cover: 'http://p3.music.126.net/J64E-TC6Pp9j8-98cwgReg==/109951165277202576.jpg?param=300x300',
+        lrc: 'https://photos.tfchen.cn/lrc/Miracles-Coldplay.lrc'
+      }
     ]
 });
